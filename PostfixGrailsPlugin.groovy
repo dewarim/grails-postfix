064d471ad71899163677bf3a87339f7c9ac04f6d
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-01-27 12:28:41 +0100

    Plugin version++

--- PostfixGrailsPlugin.groovy
+++ PostfixGrailsPlugin.groovy
@@ -17,7 +17,7 @@
  */
 class PostfixGrailsPlugin {
     // the plugin version
-    def version = "0.1.9"
+    def version = "0.2.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
